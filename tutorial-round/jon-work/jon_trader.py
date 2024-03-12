d1d3f82348579a412ff732e9313dd3dd28ae8433
Jonathan Uy <jonathan.ryan.uy@gmail.com>
2024-03-12 19:06:01 +0000

    Jon Trader - PnLs: 2702 Amethyst, 0 Starfruit

--- tutorial-round/jon-work/jon_trader.py
+++ tutorial-round/jon-work/jon_trader.py
@@ -9,7 +9,8 @@ class Trader:
     Date: 2024-03-12
     Version: 0
 
-    Amethyst: Simply makes a two-wide market around 10,000.
+    Amethyst: Simply makes a two-wide market around 10,000, of maximum size
+    such that the position limit is not violated.
 
     Starfruit (not implemented yet): Reads the order book and fills pending orders
     that are favourable, i.e. unusually high bids, or low asks.
@@ -22,31 +23,37 @@ class Trader:
 
         # Return trade requests as a map: product -> list of orders
         result = {}
+        POSITION_LIMIT = 20
 
         # Make trades on Amethysts
         product = "AMETHYSTS"
-        position = state.position[product] if (product in state.position) else 0
-        POSITION_LIMIT = 20
         orders = []
+        position = state.position[product] if (product in state.position) else 0
 
-        # Make as many orders as possible without allowing
-        # possibility of going over position limit.
-        # Fill from the outside first.
-        short_left = POSITION_LIMIT - abs(min(position, 0))
-        for price, amount in zip([10004, 10002], [10, 10]):
-            order_size = min(amount, short_left)
-            orders.append(Order(product, price, -order_size))
-            short_left -= order_size
+        # Find the maximum order size we can ask, and place it on 10002
+        short_left = -POSITION_LIMIT - min(position, 0)
+        orders.append(Order(product, 10002, short_left))
 
+        # Find the maximum order size we can bid, and place it on 9998
         long_left = POSITION_LIMIT - max(position, 0)
-        for price, amount in zip([9996, 9998], [10, 10]):
-            order_size = min(amount, long_left)
-            orders.append(Order(product, price, order_size))
-            long_left -= order_size
+        orders.append(Order(product, 9998, long_left))
 
         result[product] = orders
 
+        # Debugging just for curiosity:
+        # How often do we hit the position limit?
+        if abs(position) == POSITION_LIMIT:
+            print("We got blocked by the position limit!")
+
         # Make trades on Starfruit (not implemented)
+        product = "STARFRUIT"
+        orders = []
+        position = state.position[product] if (product in state.position) else 0
+
+        # Hack for now: since we know the price trends downwards
+        # then just short as much as possible at the very beginning
+        if position > -20:
+            orders.append(Order(product, 4995, -20))
 
         # String value holding Trader state data required
         # Delivered as state.traderData on next execution
