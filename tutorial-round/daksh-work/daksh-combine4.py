dd9a7d0dbda5c7ec7ba917068736cd1c7fbfd82d
Daksh Kaushik <daksh.kaushik3@gmail.com>
2024-03-19 17:39:32 +0000

    got backtester working

--- tutorial-round/daksh-work/daksh-combine4.py
+++ tutorial-round/daksh-work/daksh-combine4.py
@@ -2,6 +2,8 @@ from datamodel import OrderDepth, UserId, TradingState, Order
 from typing import List
 import string
 
+x = 0
+
 STARFRUIT = "STARFRUIT"
 AMETHYSTS = "AMETHYSTS"
 LIMITS = {
@@ -30,19 +32,21 @@ class Trader:
         position = Trader.get_position(state, product)
 
         if position > -15 and position < 15:
-            print("Position: " + str(position))
+            # print("Position: " + str(position))
             orders.append(Order(product, 10002, -20 - position ))
             orders.append(Order(product, 9998, 20 - position))
         elif position >=15:
-            print("Position: " + str(position))
+            # print("Position: " + str(position))
             orders.append(Order(product, 10000, -5))
             orders.append(Order(product, 10002, -15-position))
-            orders.append(Order(product, 9998, 20 - position))
+            if position != 20:
+                orders.append(Order(product, 9998, 20 - position))
         elif position <= -15:
-            print("Position: " + str(position))
+            # print("Position: " + str(position))
             orders.append(Order(product, 10000, 5))
             orders.append(Order(product, 9998, 15-position))
-            orders.append(Order(product, 10002, -20 - position))
+            if position != -20:
+                orders.append(Order(product, 10002, -20 - position))
         return orders
 
 
@@ -54,21 +58,21 @@ class Trader:
         traderData = state.traderData
         orders: List[Order] = []
 
-        newBid = max(order_depth.buy_orders.keys())
-        newAsk = min(order_depth.sell_orders.keys())
-        bidSize = order_depth.buy_orders[newBid]
-        askSize = order_depth.sell_orders[newAsk]
+        newBid = int(max(order_depth.buy_orders.keys()))
+        newAsk = int(min(order_depth.sell_orders.keys()))
+        # bidSize = order_depth.buy_orders[newBid]
+        # askSize = order_depth.sell_orders[newAsk]
             
         if traderData != "":
             #Check bid or ask has moved by a lot
             oldBidAsk = traderData.split(",")
             oldBid, oldAsk = int(oldBidAsk[0]), int(oldBidAsk[1])
-            if newBid > oldBid + 4:
-                orders.append(Order(product, newBid, max(-bidSize,-20-position)))
-                print('I WANT TO SELL', oldBid, newBid)
-            if newAsk < oldAsk - 4:
-                orders.append(Order(product, newAsk, min(-askSize,20 - position)))
-                print('I WANT TO BUY', oldAsk, newAsk)
+            if newBid > oldBid + 3 and position > -20:
+                orders.append(Order(product, newBid, -20-position))
+                # print('I WANT TO SELL', oldBid, newBid)
+            if newAsk < oldAsk - 3 and position < 20:
+                orders.append(Order(product, newAsk, 20 - position))
+                # print('I WANT TO BUY', oldAsk, newAsk)
 
         traderData = str(newBid)+","+ str(newAsk)
         return orders, traderData
@@ -77,12 +81,12 @@ class Trader:
     def run(self, state: TradingState):
         
         
-        print("traderData: " + state.traderData)
+        # print("traderData: " + state.traderData)
 
         result = {}
 
-        amethyst_orders = Trader.AmethystTrades(state)        
-        result[AMETHYSTS] = amethyst_orders
+        # amethyst_orders = Trader.AmethystTrades(state)        
+        # result[AMETHYSTS] = amethyst_orders
         
 
         starfruit_orders, traderData = Trader.StarfruitTrades(state)
@@ -91,9 +95,16 @@ class Trader:
     
 		    # String value holding Trader state data required. 
             # It will be delivered as TradingState.traderData on next execution.
-            # Sample conversion request. Check more details below. 
-        
+            # Sample conversion request. Check more details below.
         conversions = 1
 
         return result, conversions, traderData
+    
+if __name__ == "__main__":
+    import tools.backtester as backtester
+    import tools.log_parser as log_parser
+    log = log_parser.parse_log("tutorial-round\daksh-work\prober.log", parse_trader_log_as_object=True)
+    for x in range(1, 11):
+        print(x)
+        print(backtester.backtest(Trader().run, log))
     
